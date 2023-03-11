e20b524be11a1290ccdeb235e001caa6ac1ed9e0
Luis Plaza Jr <81935916+plazluis@users.noreply.github.com>
2023-03-11 18:50:23 -0500

    connected firebase

--- app/src/main/java/com/example/breath_ride/SoberActivity.java
+++ app/src/main/java/com/example/breath_ride/SoberActivity.java
@@ -3,13 +3,17 @@ package com.example.breath_ride;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SoberActivity extends AppCompatActivity {
+    private TextView sober_message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sober);
+
+        sober_message = findViewById(R.id.soberMessage);
 
 
     }
