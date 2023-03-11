25e6c2d7547e349bd98b6bb9d0c56fe5c5fcbcfd
Luis Plaza Jr <81935916+plazluis@users.noreply.github.com>
2023-03-11 17:56:39 -0500

    Sober activity screen

---
+++ app/src/main/java/com/example/breath_ride/SoberActivity.java
@@ -0,0 +1,16 @@
+package com.example.breath_ride;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SoberActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sober);
+
+
+    }
+}
