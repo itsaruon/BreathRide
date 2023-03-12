e4140a0400e02d435b1118092f18ef14d108578f
Luis Plaza Jr <81935916+plazluis@users.noreply.github.com>
2023-03-11 20:59:01 -0500

    read data from database

--- app/src/main/java/com/example/breath_ride/MainActivity.java
+++ app/src/main/java/com/example/breath_ride/MainActivity.java
@@ -1,15 +1,59 @@
 package com.example.breath_ride;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //
+
         setContentView(R.layout.activity_main);
+
+        // Initialize the Firebase SDK
+        FirebaseApp.initializeApp(this);
+
+        // Get a reference to the Realtime Database
+        DatabaseReference databaseRef = FirebaseDatabase.getInstance().getReference();
+
+        // Attach a ValueEventListener to read data from the database
+        databaseRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d(TAG, "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+
+        // Write data to the database - TEST
+        databaseRef.setValue("Hello, World!");
+
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
     }
 }
